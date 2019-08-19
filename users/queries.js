cce4e7ee5719c25a7187b8ed977fee206a754840
Greg Jordan <gregljordan@gmail.com>
2019-08-19 18:41:15 -0400

    made first get route

--- users/queries.js
+++ users/queries.js
@@ -1,5 +1,25 @@
 const db = require('../database/dbConfig.js')
 
 module.exports = {
+    find,
+    findBy,
+    addUsers
+}
+
+function find() {
+    return db('users').select('id', 'username', 'password');
+}
+
+function findBy(user) {
+    return db('users').where(user);
+}
+
+function addUsers(user) {
+   return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+        const [user] = ids;
+        return findById(id);
+    })
 
 }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -5,5 +5,15 @@ const queries = require('./queries');
 const router = express.Router();
 
 
-
+router.get('/api/users', (req, res) => {
+    
+    queries
+    .find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "There was an error retrieving the users" });
+    })
+}) 
 module.exports = router; 
